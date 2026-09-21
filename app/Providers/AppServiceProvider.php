d80536e8374336b7de1f4df1afd36fa4c9e08fc6
ikhsan5117 <ikhsan5117@users.noreply.github.com>
2026-09-21 13:47:52 +0700

    fix: force https for cloud assets and include complete starter database with all records

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        } elseif (isset($_SERVER['HTTP_HOST']) && str_contains($_SERVER['HTTP_HOST'], 'github.dev')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        } elseif (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
